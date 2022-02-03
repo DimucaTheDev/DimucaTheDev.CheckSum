9c7394bcd9294bd2c3287e1834621b587ede8738
DimucaTheDev <74795935+DimucaTheDev@users.noreply.github.com>
2022-02-03 23:18:51 +0300

    Добавьте файлы проекта.

---
+++ CheckSum/Class1.cs
@@ -0,0 +1,126 @@
+﻿using CR = System.Security.Cryptography;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DimucaTheDev.HashSum
+{
+    public class CheckSum
+    {
+        /// <summary>
+        /// Returns SHA1 hash code as a string
+        /// </summary>
+        /// <param name="file">File location</param>
+        /// <returns>System.String</returns>
+        /// <exception cref="System.IO.FileNotFoundException"></exception>
+        public static string SHA1(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    byte[] hash = sha1.ComputeHash(bs);
+                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                    return formatted.ToString();
+                }
+            }
+        }
+        /// <summary>
+        /// Returns MD5 hash code as a string
+        /// </summary>
+        /// <param name="file">File location</param>
+        /// <returns>System.String</returns>
+        /// <exception cref="System.IO.FileNotFoundException"></exception>
+        public static string MD5(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (MD5 md5 = System.Security.Cryptography.MD5.Create())
+                {
+                    byte[] hash = md5.ComputeHash(bs);
+                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                    return formatted.ToString();
+                }
+            }
+        }
+        /// <summary>
+        /// Returns SHA256 hash code as a string
+        /// </summary>
+        /// <param name="file">File location</param>
+        /// <returns>System.String</returns>
+        /// <exception cref="System.IO.FileNotFoundException"></exception>
+        public static string SHA256(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (SHA256 sha256 = System.Security.Cryptography.SHA256.Create())
+                {
+                    byte[] hash = sha256.ComputeHash(bs);
+                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                    return formatted.ToString();
+                }
+            }
+        }
+        /// <summary>
+        /// Returns SHA512 hash code as a string
+        /// </summary>
+        /// <param name="file">File location</param>
+        /// <returns>System.String</returns>
+        /// <exception cref="System.IO.FileNotFoundException"></exception>
+        public static string SHA512(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (CR.SHA512 sha512 = CR.SHA512.Create())
+                {
+                    byte[] hash = sha512.ComputeHash(bs);
+                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                    return formatted.ToString();
+                }
+            }
+        }
+        /// <summary>
+        /// Returns SHA384 hash code as a string
+        /// </summary>
+        /// <param name="file">File location</param>
+        /// <returns>System.String</returns>
+        /// <exception cref="System.IO.FileNotFoundException"></exception>
+        public static string SHA384(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                using (CR.SHA384 sha384 = CR.SHA384.Create())
+                {
+                    byte[] hash = sha384.ComputeHash(bs);
+                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
+                    foreach (byte b in hash)
+                    {
+                        formatted.AppendFormat("{0:X2}", b);
+                    }
+                    return formatted.ToString();
+                }
+            }
+        }
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,7 @@
+﻿using DimucaTheDev.HashSum;
+
+Console.WriteLine(CheckSum.SHA1(@"C:\Users\DimucaTheDev\Pictures\сапер.png"));
+Console.WriteLine(CheckSum.MD5(@"C:\Users\DimucaTheDev\Pictures\сапер.png"));
+Console.WriteLine(CheckSum.SHA256(@"C:\Users\DimucaTheDev\Pictures\сапер.png"));
+Console.WriteLine(CheckSum.SHA512(@"C:\Users\DimucaTheDev\Pictures\сапер.png"));
+Console.WriteLine(CheckSum.SHA384(@"C:\Users\DimucaTheDev\Pictures\сапер.png"));
